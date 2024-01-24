ccd73902010e68459f4635bf6b25c58ef0ad4bbd
Reidar Nilsen <4reidar@gmail.com>
2024-01-24 22:08:50 +0100

    Add project files.

---
+++ ArtistClient/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistClient.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
---
+++ ArtistClient/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using ArtistClient.Models;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ArtistClient
+{
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            
+            await GetArtistsAsync();
+            await AddArtistAsync(new Artist { ArtistName = "Reidar"});
+        }
+
+        static async Task GetArtistsAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7146/artists");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        
+        static async Task AddArtistAsync(Artist artist)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(artist);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("https://localhost:7096/artists", content);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
